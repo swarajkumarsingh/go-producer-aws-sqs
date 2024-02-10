7362263efbaf6255799b6542ae84047374a1b5c2
Swaraj Kumar Singh <89764448+swarajkumarsingh@users.noreply.github.com>
2024-02-10 21:45:29 +0530

    added Readme.md

--- run.sh
+++
@@ -1,2 +0,0 @@
-docker compose down
-make run STAGE=dev SQS_URL=https://sqs.ap-south-1.amazonaws.com/248229095548/email-queue.fifo AWS_REGION=ap-south-1 AWS_ACCESS_KEY=AKIATTS43NB6MYAXOGJU AWS_SECRET_ACCESS_KEY=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 AWS_TOKEN=
